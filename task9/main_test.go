aef8241900e582de362319717e6330ae9a137f7b
kry shitenkova <shitenkovakris@mail.ru>
2024-02-07 13:40:33 +0200

    do unit test for task9

---
+++ task9/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Case1(t *testing.T) {
+	array := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	expected := []int{1, 4, 7}
+
+	actual := FindElementsWhichAreInTheLeftSideOfTheMatrix(array)
+
+	assert.Equal(t, expected, actual)
+}
