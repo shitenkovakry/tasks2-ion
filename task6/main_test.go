8565aa329a96cd515f60395e97e9509ccb24bbb4
kry shitenkova <shitenkovakris@mail.ru>
2024-02-07 12:59:27 +0200

    do unit test for task6

---
+++ task6/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Case1(t *testing.T) {
+	array := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	expected := [][]int{
+		{6},
+		{9, 8},
+	}
+
+	actual := FindElementsWhichLowerSideDiagonal(array)
+
+	assert.Equal(t, expected, actual)
+}
