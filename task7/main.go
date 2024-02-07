8ed129321fccd7dcb588d8c794a0dd9648cb6e11
kry shitenkova <shitenkovakris@mail.ru>
2024-02-07 13:10:12 +0200

    do unit test for task7

--- task7/main.go
+++ task7/main.go
@@ -1,6 +1,6 @@
 package main
 
-func FindElementsWhichareAtTheTopOfTheMatrix(array [][]int) []int {
+func FindElementsWhichAreAtTheTopOfTheMatrix(array [][]int) []int {
 	arrayWithResult := []int{}
 	lenOfArray := len(array)
 
