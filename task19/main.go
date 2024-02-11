067fd97d23761cd37e202de1e1d1d84dd7540f57
kry shitenkova <shitenkovakris@mail.ru>
2024-02-11 15:43:44 +0200

    implement clockwise with unit tests beginning from [0,0]

---
+++ task19/main.go
@@ -0,0 +1,78 @@
+package main
+
+type Direction int
+
+const (
+	Clockwise Direction = iota + 1
+	CounterClockwise
+)
+
+type SpiralArgument struct {
+	StartI    int
+	StartJ    int
+	Direction Direction
+}
+
+func SpiralOrder(array [][]int, spiralArgument SpiralArgument) []int {
+	left := 0
+	right := len(array) - 1
+	result := []int{}
+
+	for index := 0; index <= len(array)/2; index++ {
+		if left > 0 {
+			result = append(result, Walk(array, left, left-1, left, right)...)
+		} else {
+			result = append(result, Walk(array, left, left, left, right)...)
+		}
+
+		result = append(result, Walk(array, left, right, right, right)...)
+
+		if right != left {
+			result = append(result, Walk(array, right, right, right, left)...)
+			result = append(result, Walk(array, right, left, left+1, left)...)
+		}
+
+		left++
+		right--
+
+		if left > right {
+			break
+		}
+	}
+
+	return result
+}
+
+func Walk(array [][]int, x1, y1, x2, y2 int) []int {
+	result := []int{}
+
+	if x1 == x2 && y1 < y2 {
+		for index := y1; index < y2; index++ {
+			result = append(result, array[x1][index])
+		}
+	}
+
+	if x1 == x2 && y1 > y2 {
+		for index := y1; index > y2; index-- {
+			result = append(result, array[x1][index])
+		}
+	}
+
+	if y1 == y2 && x1 > x2 {
+		for index := x1; index > x2; index-- {
+			result = append(result, array[index][y1])
+		}
+	}
+
+	if y1 == y2 && x1 < x2 {
+		for index := x1; index < x2; index++ {
+			result = append(result, array[index][y1])
+		}
+	}
+
+	if x1 == x2 && y1 == y2 {
+		result = append(result, array[x1][y1])
+	}
+
+	return result
+}
