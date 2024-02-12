694659a2ffe8101e09215f3c4f7ec49b913783d6
kry shitenkova <shitenkovakris@mail.ru>
2024-02-12 17:06:36 +0200

    do unit test for task24

---
+++ task24/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Case1(t *testing.T) {
+	arrayOne := [][]string{
+		{"a3", "b1"},
+		{"a2", "b3"},
+		{"a1", "b2"},
+	}
+
+	arrayTwo := [][]string{
+		{"a3", "b3"},
+		{"a2", "b3"},
+		{"a1", "b2"},
+		{"a1", "b1"},
+	}
+
+	expected := [][]string{
+		{"a3", "b1"},
+	}
+
+	actual := FindTheDifferenceOfTwoMatrices(arrayOne, arrayTwo)
+
+	assert.Equal(t, expected, actual)
+}
