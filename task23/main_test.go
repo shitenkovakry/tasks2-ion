ed64f28eb46bdc7b9a6e0af234eb426618521825
kry shitenkova <shitenkovakris@mail.ru>
2024-02-12 16:56:53 +0200

    do unit test for task23

---
+++ task23/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Case2(t *testing.T) {
+	arrayOne := [][]string{
+		{"a3", "b1"},
+		{"a2", "b3"},
+		{"a1", "b2"},
+	}
+
+	arrayTwo := [][]string{
+		{"a3", "b3"},
+		{"a2", "b3"},
+		{"a1", "b2"},
+		{"a1", "b1"},
+	}
+
+	expected := [][]string{
+		{"a2", "b3"},
+		{"a1", "b2"},
+	}
+
+	actual := FindTheIntersectionOfTwoMatrices(arrayOne, arrayTwo)
+
+	assert.Equal(t, expected, actual)
+}
